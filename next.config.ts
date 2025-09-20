aadd2774569c4ed76762056d5433ba5dd05db4fc
takaaki <soci0926@gmail.com>
2025-09-20 14:53:23 +0900

    chanage LP

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,20 @@ const nextConfig: NextConfig = {
       },
 
     images: {
-      domains: ['vexpostudio.s3.ap-northeast-1.amazonaws.com','vexpo.s3.ap-northeast-1.amazonaws.com'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'vexpostudio.s3.ap-northeast-1.amazonaws.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'vexpo.s3.ap-northeast-1.amazonaws.com',
+        },
+      ],
       // 可能なら出力フォーマットをAVIF優先に
-    formats: ['image/avif', 'image/webp'],
+      formats: ['image/avif', 'image/webp'],
+      deviceSizes: [360, 420, 640, 768, 1024, 1200], // 主要ブレイクポイント
+      imageSizes: [320, 480, 640, 800], // 固定幅用
     },
 };
 
